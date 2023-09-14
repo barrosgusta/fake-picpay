dc122bf9396905cee085ee7aaf95e13ff3113dc8
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-09-14 16:36:25 -0300

    Initial Commit

---
+++ fake-picpay/fake_picpayApp.swift
@@ -0,0 +1,17 @@
+//
+//  fake_picpayApp.swift
+//  fake-picpay
+//
+//  Created by Gustavo Barros da Silveira on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct fake_picpayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
